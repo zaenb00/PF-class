d536d04c41a4666a219f8c1c9e0dfdf5418bada8
zaenb00 <zaenbishtiaq53@gmail.com>
2023-09-15 12:24:40 -0700

    message

---
+++ doraemon.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "                 #########                                                          "<< endl;
+cout << "               #  .**.**.  #                                                        "<< endl;
+cout << "              #   *O *O *   #                                                       "<< endl;
+cout << "             #     **o**     #                                                      "<< endl;
+cout << "             #   ____|____   #  ****   ***  ****   ***  **** **   **  ***  **   *   "<< endl;
+cout << "              #  .       .  #   *   * *   * *___* *   * *__  * * * * *   * * *  *   "<< endl;
+cout << "               #  ' ___ '  #    *   * *   * *  *  ***** *    *  *  * *   * *  * *   "<< endl; 
+cout << "                 #########      ****   ***  *   * *   * **** *     *  ***  *   **   "<< endl;
+cout << "               #   '.O.'   #                                                        "<< endl;
+cout << "              #  #       #  #                                                       "<< endl;
+cout << "             #  #  _____  #  #                                                      "<< endl;
+cout << "            (__)#  . _ .  #(__)                                                     "<< endl;
+cout << "                #         #                                                         "<< endl;
+cout << "                #___ | ___#                                                         "<< endl;    
+cout << "                (___) (___)                                                         ";
+}    
